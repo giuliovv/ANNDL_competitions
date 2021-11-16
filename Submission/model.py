f06fdae7a0911e84de1ca050a2a08efe4c2fc4fd
sikora-toma <ts50570@fer.hr>
2021-11-16 11:02:57 +0100

    Added .numpy() before prediction

--- Submission/model.py
+++ Submission/model.py
@@ -1,12 +1,12 @@
 import os
 import tensorflow as tf
 
-class Model:
+class model:
     def __init__(self, path):
         self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel/model1_20'))
         return
 
     def predict(self, X):
-        X = tf.keras.applications.vgg16.preprocess_input(X)
+        X = tf.keras.applications.vgg16.preprocess_input(X.numpy())
         prediction = self.model.predict(X)
         return tf.argmax(prediction, axis=-1)
